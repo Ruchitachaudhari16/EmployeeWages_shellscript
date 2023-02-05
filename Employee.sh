52591f643c7325fc97aba181ae54abb81d3aea05
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-05 12:16:00 -0500

    Part time Wages Added

--- Employee.sh
+++ Employee.sh
@@ -1,2 +1,21 @@
-echo "Welcome to Employee wage Computation Program"
-echo  "Welcome to Indian Army"
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+
+if [ $isFullTime -eq $randomCheck ]
+then
+    empHrs=8;
+
+elif [ $isPartTime -eq $randomCheck ]
+then
+    empHrs=4;
+
+salary=$(( $empHrs*$empRatePerHr ));
+echo $salary;
+
+else 
+     empHrs=0;
+echo $salary;
+
+fi
