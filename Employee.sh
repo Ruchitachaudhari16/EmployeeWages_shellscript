1a7e7eaa5ab545db63993364f96503e69d9a32a9
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-05 23:36:59 -0500

    Daily Wages Added

--- Employee.sh
+++ Employee.sh
@@ -5,31 +5,40 @@ empRatePerHr=20;
 numWorkingDays=20;
 
 #Variables
-totalEmpHr=0;
-totalWorkingDays=0;
+totalEmpHr=0
+totalWorkingDays=0
 
 function getWorkingHours()
 {
-case $1 in
-       $isFullTime)
-     WorkingHrs=8
-         ;;
-        $isPartTime)
-           WorkingHrs=4
-          ;;
-          *)
-      WorkingHrs=1
-           ;;
 
+local $empCheck=$1
+
+case $empCheck in
+      
+ $isFullTime)
+     empHrs=8
+         ;;
+
+        $isPartTime)
+           empHrs=4
+          ;;
+
+          *)
+      empHrs=0
+           ;;
 esac
-echo$WorkingHrs
+echo $empHrs
+
 }
 
-while [[ $totalWorkingHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $numWorkingdays ]]
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $numWorkingdays ]]
 do 
-   (( totalWorkingDays++ ))
-      WorkHours=$(getWorkingHrs $((RANDOM%3)));
-      totaWorkingHrs=$(($totalWorkingHrs + $WorkingHrs));
+   ((totalWorkingDays++))
+      empCheck= $((RANDOM%3));
+      
+empHrs="$( getWorkingHrs $empCheck )"
+      totalEmpHrs=$(( $totalEmpHr + $empHrs ))
+      dailyWage[$totalWorkingDays]=$(($empHrs*($empRatePerHr)))
  done
-WorkHrs=$(( $totalWorkingHrs+$empRatePerHr ));
-echo "Working Hrs is :$WorkHrs";
+totalSalary=$(($totalEmpHr*$empRatePerHr));
+echo ${dailyWage[@]}
